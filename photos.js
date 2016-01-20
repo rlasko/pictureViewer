0cecc814bbb95b9451b6eca6bb316f15b2a1f70f
Rae  Lasko <RaeLasko@Raes-MBP.wv.cc.cmu.edu>
2016-01-20 14:56:39 -0500

    Added source files

---
+++ photos.js
@@ -0,0 +1,71 @@
+var api_key = '8665d22603b0a80df271bfeeddce0249';
+var baseURL = ' https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=' + api_key + '&format=json&nojsoncallback=1';
+document.addEventListener('DOMContentLoaded', function () {
+  requestPhotos();
+}, false);
+
+//requests photos from api
+function requestPhotos () {
+  requestHTTP = new XMLHttpRequest();
+  console.log('requesting photos');
+    if (!requestHTTP) {
+      console.error('Could not be requested');
+    }
+    requestHTTP.onreadystatechange = checkRequestStatus;
+    requestHTTP.open('GET', baseURL, true);
+    console.log('getting done');
+    requestHTTP.send(null);
+}
+
+//handles errors and checks the request status
+function checkRequestStatus () {
+  if (requestHTTP.readyState === XMLHttpRequest.DONE) {
+    if (requestHTTP.status === 200){
+      console.log('status = 200');
+      res = JSON.parse(requestHTTP.responseText)
+      parsePhotos(res.photos.photo);
+    }else {
+      console.error('Error: The response could not be completed');
+    }
+  }
+
+function parsePhotos(photosArray) {
+  var txtStr = ''
+
+  //creates image URL from photosArray
+  for (var i = 0; i < photosArray.length; i++) {
+    var photo = photosArray[i];
+    var imgUrl = "http://farm" + photo.farm + ".static.flickr.com/" +
+        photo.server + "/" + photo.id + "_" + photo.secret + "_";
+    var imgUrlWSize = imgUrl + "s.jpg"
+    var title =  photo.title.replace(/'/g, "&apos;").replace(/"/g, "&quot;");
+    txtStr +=  '<a href="#" class="photo" >' + '<img alt="'+ title  +
+    '"src="' + imgUrlWSize + '"/>' + '</a>';
+  }
+  document.getElementById('images').innerHTML= txtStr;
+  var domPhotos = document.getElementsByClassName('photo')
+
+  //waits for click on img and calls func that gets larger image
+  for (var i = 0; i<domPhotos.length;i++) {
+    (function(i){
+    domPhotos[i].addEventListener('click', function () {
+      return chooseImg(photosArray,i);
+    }, false);
+  }(i));
+  }
+}
+
+//gets URL of larger img and calls func to display it
+function chooseImg (photosArray,i) {
+    var photo = photosArray[i];
+    var imgUrl = "http://farm" + photo.farm + ".static.flickr.com/" +
+        photo.server + "/" + photo.id + "_" + photo.secret + "_" + "c.jpg"
+    return (openLightBox(imgUrl))
+}
+
+function openLightBox (imgUrl) {
+  document.getElementById('photoLightbox').style.display='inline';
+  document.getElementById('lightBoxContents').innerHTML = '<img src=' + imgUrl + '/>';
+}
+
+}
